bb6053eb0a8ccd462f0eafcd0ee3edc7cda0b918
popschool <popschool@debian>
2018-04-29 11:50:39 +0200

    Add scroll to top js code and html code

---
+++ scrolltotop.js
@@ -0,0 +1,17 @@
+// HTML
+
+      <a href="javascript:" id="top"><i class="icon-chevron-up"></i></a>
+
+// ===== Scroll to Top ====
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 100) {    // If page is scrolled more than 50px
+        $('#top').fadeIn("fast");       // Fade in the arrow
+    } else {
+        $('#top').fadeOut("fast");      // Else fade out the arrow
+    }
+});
+$('#top').click(function() {            // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                   // Scroll to top of body
+    }, 500);
+});
